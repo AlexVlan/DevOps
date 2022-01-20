d8ce59809b16b8baa7dbb29308db59eca69179b8
Alex_Vlan <67640029+AlexVlan@users.noreply.github.com>
2022-01-20 15:03:35 +0300

    Add files via upload

---
+++ make_web_srv.tf
@@ -0,0 +1,51 @@
+#---------------------------------------------------------------------------------------
+#My Terraform
+#Build WebServer Bootsrap
+#---------------------------------------------------------------------------------------
+provider "aws" {
+    region - "eu-centeral-1" 
+}
+
+resource "aws-instance" "WebServer" {
+    ami = "ami-0sdfdjn"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_tls] 
+    #add instance from swc groupe
+}
+
+#create new security groupe
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
